a3216c6e1ae089f1e2edf07153ec23112dbc0721
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-30 10:32:02 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const btn = document.querySelector("#button");
+    btn.addEventListener("Click", function() {
+        console.log("Event Triggered");
+    });
 }
